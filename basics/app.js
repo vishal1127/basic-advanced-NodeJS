f8c2be0d5574765adfd4aa60552b2870f22c01ea
vishal2796 <vishal.pb.27@gmail.com>
2022-12-14 18:00:12 +0530

    accessing different routes

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,37 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	console.log('Hello Vishal');
+	console.log(req.url, req.method, req.headers);
+	const url = req.url;
+	if (url === '/') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<head><title>My first node page</title></head>');
+		res.write('<body><h1>Hello from Node JS</h1></body>');
+		res.write('</html>');
+		return res.end();
+	} else if (url === '/home') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<head><title>Homepage</title></head>');
+		res.write('<body><h1>Welcome home</h1></body>');
+		res.write('</html>');
+		return res.end();
+	} else if (url === '/about') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<head><title>About us page</title></head>');
+		res.write('<body><h1>Welcome to About Us page</h1></body>');
+		res.write('</html>');
+		return res.end();
+	} else if (url === '/node') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<head><title>Node page</title></head>');
+		res.write('<body><h1Welcome to my Node Js project</h1></body>');
+		res.write('</html>');
+		return res.end();
+	}
 });
 
 server.listen(4000);
