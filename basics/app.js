215084a365c76d84327d533e40802e8ee88f6bfc
vishal2796 <vishal.pb.27@gmail.com>
2022-12-16 16:51:11 +0530

    intergrating express.js

--- basics/app.js
+++ basics/app.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const routes = require('./routes.js');
+const http = require("http");
+const express = require("express");
 
-const server = http.createServer(routes.handler);
+const app = express();
+app.use((req, res, next) => {
+  console.log("inside the middleware");
+  next(); // allows the request to continue to the next middleware in line
+});
 
-server.listen(4000);
+app.use((req, res, next) => {
+  console.log("insdie another middlerware");
+  res.send({ key1: "value" });
+});
+
+app.listen(3000);
