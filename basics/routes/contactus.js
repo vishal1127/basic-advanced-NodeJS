8a160faf774603746aca49f59ef31dfada4e02db
vishal2796 <vishal.pb.27@gmail.com>
2022-12-18 20:08:29 +0530

    dynamic routes

--- basics/routes/contactus.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const contactUsController = require("../controllers/contactus");
-
-router.get("/contactus", contactUsController.getContactUs);
-
-module.exports = router;
--- basics/routes/success.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const contactUsController = require("../controllers/contactus");
-
-router.post("/success", contactUsController.postSuccess);
-
-module.exports = router;
