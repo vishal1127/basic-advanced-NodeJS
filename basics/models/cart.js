e04a3628083934d5a50331e9fafca893a1642342
vishal2796 <vishal.pb.27@gmail.com>
2022-12-20 16:51:25 +0530

    POST Query

---
+++ basics/models/cart.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+
+module.exports = class Cart {
+  static addProduct(id, prodPrice) {
+    //Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      //analyze the cart=> find existing product
+      const existingProductIndex = cart.products.findIndex((p) => p.id === id);
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      //add new product /increase the quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +prodPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
